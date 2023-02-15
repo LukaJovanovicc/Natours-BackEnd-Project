e077c6d9d9c2d68d6802bdbdcc894315ec8b79dc
hejzzz <jluka137@gmail.com>
2023-02-15 17:27:19 +0100

    Implemented CORS

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globarErrorHandler = require('./controllers/errorController');
@@ -24,8 +25,12 @@ app.enable('trust proxy');
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(cors());
+
 //Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.options('*', cors());
 
 //Security Http headers
 // app.use(helmet());
