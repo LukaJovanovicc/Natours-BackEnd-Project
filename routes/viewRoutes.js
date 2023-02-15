851e51012a2a9a9cc55f355f705fe88fcf5f4c2e
hejzzz <jluka137@gmail.com>
2023-02-15 19:36:26 +0100

    Improved stripe implementation

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -5,12 +5,7 @@ const viewsController = require('../controllers/viewsController');
 const authController = require('../controllers/authController');
 const bookingController = require('../controllers/bookingController');
 
-router.get(
-  '/',
-  bookingController.createBookingCheckout,
-  authController.isLoggedIn,
-  viewsController.getOverview
-);
+router.get('/', authController.isLoggedIn, viewsController.getOverview);
 router.get('/tours/:slug', authController.isLoggedIn, viewsController.getTour);
 router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
 router.get('/me', authController.protect, viewsController.getAccount);
