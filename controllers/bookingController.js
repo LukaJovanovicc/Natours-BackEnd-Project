f379a998d4b90aeab272032404deb5a6fed27864
hejzzz <jluka137@gmail.com>
2023-02-16 12:53:20 +0100

    stripe fix

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -17,20 +17,14 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     client_reference_id: req.params.tourId,
     line_items: [
       {
-        price_data: {
-          unit_amount: tour.price * 100,
-          currency: 'usd',
-          product_data: {
-            name: `${tour.name} Tour`,
-            description: tour.summary,
-            images: [
-              `${req.protocol}://${req.get('host')}/img/tours/${
-                tour.imageCover
-              }`,
-            ],
-          },
-        },
+        price: tour.price * 100,
         quantity: 1,
+        currency: 'usd',
+        name: `${tour.name} Tour`,
+        description: tour.summary,
+        images: [
+          `${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`,
+        ],
       },
     ],
   });
@@ -44,7 +38,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.line_items[0].price_data.unit_amount / 100;
+  const price = session.line_items[0].price / 100;
   await Booking.create({ tour, user, price });
 };
 
